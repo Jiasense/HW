ac36fb55a6b30fe6a3287a1307fa78af8e82f2dc
jiasen <jiasen@jiasendeMacBook-Pro.local>
2025-03-26 11:29:58 +0800

    test delete

---
+++ DeepFlame_Academic_Test/build/CMakeFiles/dfacademic.dir/DependInfo.cmake
@@ -0,0 +1,71 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/1230/DeepFlame_Academic/src/basic/basic_kernel.cu" "CMakeFiles/dfacademic.dir/src/basic/basic_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/basic/basic_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/boundary/boundary.cu" "CMakeFiles/dfacademic.dir/src/boundary/boundary.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/boundary/boundary.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/boundary/boundary_kernel.cu" "CMakeFiles/dfacademic.dir/src/boundary/boundary_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/boundary/boundary_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/chemistry/chemistry_solver.cu" "CMakeFiles/dfacademic.dir/src/chemistry/chemistry_solver.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/chemistry/chemistry_solver.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/chemistry/chemistry_solver_kernel.cu" "CMakeFiles/dfacademic.dir/src/chemistry/chemistry_solver_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/chemistry/chemistry_solver_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/common.cu" "CMakeFiles/dfacademic.dir/src/common.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/common.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfEqns/dfEEqn.cu" "CMakeFiles/dfacademic.dir/src/dfEqns/dfEEqn.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfEqns/dfEEqn.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfEqns/dfThermo.cu" "CMakeFiles/dfacademic.dir/src/dfEqns/dfThermo.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfEqns/dfThermo.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfEqns/dfThermo_kernel.cu" "CMakeFiles/dfacademic.dir/src/dfEqns/dfThermo_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfEqns/dfThermo_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfEqns/dfUEqn.cu" "CMakeFiles/dfacademic.dir/src/dfEqns/dfUEqn.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfEqns/dfUEqn.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfEqns/dfUEqn_kernel.cu" "CMakeFiles/dfacademic.dir/src/dfEqns/dfUEqn_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfEqns/dfUEqn_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfEqns/dfYEqn.cu" "CMakeFiles/dfacademic.dir/src/dfEqns/dfYEqn.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfEqns/dfYEqn.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfEqns/dfYEqn_kernel.cu" "CMakeFiles/dfacademic.dir/src/dfEqns/dfYEqn_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfEqns/dfYEqn_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfEqns/dfpEqn.cu" "CMakeFiles/dfacademic.dir/src/dfEqns/dfpEqn.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfEqns/dfpEqn.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfEqns/dfpEqn_kernel.cu" "CMakeFiles/dfacademic.dir/src/dfEqns/dfpEqn_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfEqns/dfpEqn_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfEqns/dfrhoEqn.cu" "CMakeFiles/dfacademic.dir/src/dfEqns/dfrhoEqn.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfEqns/dfrhoEqn.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfNcclBase.cu" "CMakeFiles/dfacademic.dir/src/dfNcclBase.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfNcclBase.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/dfacademic.cu" "CMakeFiles/dfacademic.dir/src/dfacademic.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/dfacademic.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/fvc/fvc_kernel.cu" "CMakeFiles/dfacademic.dir/src/fvc/fvc_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/fvc/fvc_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/fvm/fvm_kernel.cu" "CMakeFiles/dfacademic.dir/src/fvm/fvm_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/fvm/fvm_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/interpolation/interpolation.cu" "CMakeFiles/dfacademic.dir/src/interpolation/interpolation.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/interpolation/interpolation.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/interpolation/interpolation_kernel.cu" "CMakeFiles/dfacademic.dir/src/interpolation/interpolation_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/interpolation/interpolation_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/amgx_solver/AmgxXSolver.cu" "CMakeFiles/dfacademic.dir/src/matrix/amgx_solver/AmgxXSolver.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/amgx_solver/AmgxXSolver.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_base.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_base.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_base.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_base_kernel.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_base_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_base_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/CSR/dfCSRJacobiSmoother.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfCSRJacobiSmoother.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfCSRJacobiSmoother.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/CSR/dfCSRSRJSmoother.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfCSRSRJSmoother.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfCSRSRJSmoother.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/CSR/dfDiagonalCSRPreconditioner.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfDiagonalCSRPreconditioner.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfDiagonalCSRPreconditioner.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/CSR/dfDiagonalCSRSolver.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfDiagonalCSRSolver.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfDiagonalCSRSolver.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/CSR/dfGAMGCSRPreconditioner.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfGAMGCSRPreconditioner.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfGAMGCSRPreconditioner.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/CSR/dfJacobiCSRPreconditioner.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfJacobiCSRPreconditioner.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfJacobiCSRPreconditioner.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/CSR/dfPBiCGStabCSRSolver.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfPBiCGStabCSRSolver.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfPBiCGStabCSRSolver.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/CSR/dfPCGCSRSolver.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfPCGCSRSolver.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/CSR/dfPCGCSRSolver.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/ELL/dfDiagonalELLSolver.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfDiagonalELLSolver.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfDiagonalELLSolver.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/ELL/dfELLJacobiSmoother.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfELLJacobiSmoother.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfELLJacobiSmoother.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/ELL/dfELLSRJSmoother.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfELLSRJSmoother.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfELLSRJSmoother.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/ELL/dfGAMGELLPreconditioner.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfGAMGELLPreconditioner.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfGAMGELLPreconditioner.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/ELL/dfPBiCGStabELLSolver.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfPBiCGStabELLSolver.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfPBiCGStabELLSolver.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/ELL/dfPCGELLSolver.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfPCGELLSolver.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/ELL/dfPCGELLSolver.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/dfSolverOpBase.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/dfSolverOpBase.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/dfSolverOpBase.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/gamg.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/gamg.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/gamg.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/matrix_solution_scheme/gamg_kernel.cu" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/gamg_kernel.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/matrix_solution_scheme/gamg_kernel.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/preconditioner.cu" "CMakeFiles/dfacademic.dir/src/matrix/preconditioner.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/preconditioner.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/smoother.cu" "CMakeFiles/dfacademic.dir/src/matrix/smoother.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/smoother.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/matrix/solver.cu" "CMakeFiles/dfacademic.dir/src/matrix/solver.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/matrix/solver.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/turbulence/LES/Sigma.cu" "CMakeFiles/dfacademic.dir/src/turbulence/LES/Sigma.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/turbulence/LES/Sigma.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/turbulence/LES/Smagorinsky.cu" "CMakeFiles/dfacademic.dir/src/turbulence/LES/Smagorinsky.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/turbulence/LES/Smagorinsky.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/turbulence/LES/kEqnLES.cu" "CMakeFiles/dfacademic.dir/src/turbulence/LES/kEqnLES.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/turbulence/LES/kEqnLES.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/turbulence/RAS/kEpsilon.cu" "CMakeFiles/dfacademic.dir/src/turbulence/RAS/kEpsilon.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/turbulence/RAS/kEpsilon.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/turbulence/TCI/PaSR.cu" "CMakeFiles/dfacademic.dir/src/turbulence/TCI/PaSR.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/turbulence/TCI/PaSR.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/turbulence/TCI/infinitelyFastChemistry.cu" "CMakeFiles/dfacademic.dir/src/turbulence/TCI/infinitelyFastChemistry.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/turbulence/TCI/infinitelyFastChemistry.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/turbulence/combustion.cu" "CMakeFiles/dfacademic.dir/src/turbulence/combustion.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/turbulence/combustion.cu.o.d"
+  "/root/1230/DeepFlame_Academic/src/turbulence/turbulence.cu" "CMakeFiles/dfacademic.dir/src/turbulence/turbulence.cu.o" "gcc" "CMakeFiles/dfacademic.dir/src/turbulence/turbulence.cu.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ DeepFlame_Academic_Test/build/CMakeFiles/dfacademic.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dfacademic.
+# This may be replaced when dependencies are built.
